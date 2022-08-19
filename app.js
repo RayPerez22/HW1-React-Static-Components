29acde605f6672f3a0ba0c80e57c59594aa7856b
RayPerez22 <raynyc2285@gmail.com>
2022-08-18 23:05:44 -0400

    final commit

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ class Aside extends React.Component {
     render(){
         return (
         <aside>
-            <nav>
-                <a>Dashboard</a>
-                <a> Widget</a>
-                <a> Reviews</a>
-                <a> Customers</a>
-                <a> Online Analysis</a>
-                <a> Setting</a>
-            </nav>
-
+            <ul>
+                <li>Dashboard</li>
+                <li> Widget</li>
+                <li> Reviews</li>
+                <li> Customers</li>
+                <li> Online Analysis</li>
+                <li> Setting</li>
+            </ul>
         </aside>
         )
     }
@@ -19,27 +18,39 @@ class Aside extends React.Component {
 
 class Reviews extends React.Component{
     render() {
-       return <section>Reviews 1281</section>       
+       return <section>
+        <h5>Reviews</h5>
+        <h1>1281</h1>
+        <h4>960</h4>
+        <h4>321</h4>
+        </section>       
     }
 }
 
 class AvgRating extends React.Component {
     render(){
-        return <section>Average Rating</section>
+        return <section>
+          <h5>Av Rating</h5>
+          <h1>4.6</h1> </section>
     }
 }
 
 class Sentiment extends React.Component {
     render(){
-        return <section>Sentiment Analysis</section>
+        return <section>
+          <h5>Sentiment Analysis</h5>
+          <h3>960</h3><br/>
+          <h3>122</h3><br/>
+          <h3>321</h3><br/>
+          </section>
     }
 }
 
 class WebVisitors extends React.Component {
     render(){
         return <section id='large'>
-            <h3>WebVisitors</h3>
-            <h2>821</h2>
+            <h5>Website Visitors</h5>
+            <h1>821</h1>
             <div></div>
 
         </section>
@@ -70,3 +81,4 @@ ReactDOM.render(
     <App />,
     document.querySelector('main')
   );
+
